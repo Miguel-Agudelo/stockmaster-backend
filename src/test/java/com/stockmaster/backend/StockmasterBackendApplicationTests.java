87894a14f5de9c8b5074658ad91e064c1a43e7e0
Miguel Agudelo <veramiguel348@gmail.com>
2025-09-09 14:44:28 -0500

    Spring initialzr

---
+++ src/test/java/com/stockmaster/backend/StockmasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmaster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
